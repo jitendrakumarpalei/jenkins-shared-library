ad267a9af93f26dcda3de0abceeddfedace776ea
jitu <jitendrakumarpalei6@gmail.com>
2023-07-28 15:44:23 +0530

    separate file

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,9 +2,5 @@
  
 def call() {
     echo "building the docker image.."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t jitendrapalei/java-maven-app:2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push jitendrapalei/java-maven-app:2.0'
-    }
+    sh 'docker build -t jitendrapalei/java-maven-app:2.0 .'        
 }
---
+++ vars/dockerPush.groovy
@@ -0,0 +1,5 @@
+#!/usr/bin/env groovy
+
+def call() {
+    sh 'docker push jitendrapalei/java-maven-app:2.0'
+}
